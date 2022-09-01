63e526c832db9a5e0ad61577d9e11d60282bd64f
Daria Dychek <dychek.d@gmail.com>
2022-09-01 17:13:11 +0200

    task3

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine ("enter integer ");
+try
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    if (a%2 == 0) 
+    {
+        Console.WriteLine (a+" - even number");
+    }
+    else 
+    {
+        Console.WriteLine (a+" - odd number");
+    }
+}
+catch
+{
+    Console.WriteLine ("Please enter integer");
+}
