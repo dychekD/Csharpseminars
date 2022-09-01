2aad7f1a5d3647b20479522758db4bb6f3c5b93c
Daria Dychek <dychek.d@gmail.com>
2022-09-01 17:07:23 +0200

    task2

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine ("enter first integer ");
+try
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine ("enter second integer ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine ("enter third integer ");
+    int c = Convert.ToInt32(Console.ReadLine());
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    Console.WriteLine ("max = "+max);
+}
+catch
+{
+    Console.WriteLine ("Please enter integers");
+}
