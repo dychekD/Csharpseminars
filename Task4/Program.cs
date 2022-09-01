fbd6b0f3fbe693431dd16cbce3303b79a454b438
Daria Dychek <dychek.d@gmail.com>
2022-09-01 17:23:36 +0200

    task4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("enter positive integer greater than 1 ");
+try
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    int even = 2;
+    while (even <= a) 
+    {
+        Console.Write (even+ ", ");
+        even = even + 2;
+    }
+}
+catch
+{
+    Console.WriteLine ("Please enter positive integer greater than 1");
+}
