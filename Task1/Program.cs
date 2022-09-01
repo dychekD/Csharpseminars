1c368cbfe6594b76d7ea8b9bb2df0f2af855947a
Daria Dychek <dychek.d@gmail.com>
2022-09-01 16:56:17 +0200

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("enter first integer ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("enter second integer ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a >= b) 
+{
+    Console.WriteLine ("max = "+a);
+}
+else 
+{
+    Console.WriteLine ("max = "+b);
+}
