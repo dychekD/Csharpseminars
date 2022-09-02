d990c2bc0ee1497040b678077e14f36c58a7ceef
Daria Dychek <dychek.d@gmail.com>
2022-09-02 20:15:28 +0200

    task5

---
+++ Task5/Program.cs
@@ -0,0 +1,13 @@
+﻿void NewTask ()
+{
+int num = new Random().Next (100, 1000);
+
+int res =  num/100;
+int res1 = num%10;
+
+Console.WriteLine(num);
+Console.WriteLine($"{res}{res1}");
+}
+
+NewTask ();
+
