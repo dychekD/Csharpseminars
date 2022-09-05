2bc50eca85fbfee54590893fc987359482f23b72
Daria Dychek <dychek.d@gmail.com>
2022-09-05 22:00:14 +0200

    task9

---
+++ Task9/Program.cs
@@ -0,0 +1,20 @@
+﻿void Delete2 (int anynumber)
+{
+    if (anynumber % 10 == anynumber) Console.WriteLine ("there is no second digit");
+    else
+    {
+        int count = 0;
+        int temp = anynumber;
+        while (temp / 10 > 9)
+        {
+            temp = temp / 10;
+            count++;
+        }
+        temp = temp / 10;
+        if (count == 0) Console.WriteLine ($"{temp}");
+        else Console.WriteLine ($"{temp}{anynumber % (Math.Pow (10, count))}");
+    }
+}
+Console.WriteLine ("enter a number");
+int anynumber = Convert.ToInt32 (Console.ReadLine());
+Delete2 (anynumber);
