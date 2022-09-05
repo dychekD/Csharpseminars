0f00a778f35909abe88f51abfaa629b3b82ec747
Daria Dychek <dychek.d@gmail.com>
2022-09-05 21:32:49 +0200

    task8

---
+++ Task8/Program.cs
@@ -0,0 +1,9 @@
+﻿void SorryForTranslit (int numProg)
+{
+    if (numProg % 10 == 0 || numProg % 10 >= 5 ) Console.WriteLine ($"{numProg} programmistov");
+    else if (numProg % 10 == 1) Console.WriteLine ($"{numProg} programmist");
+    else Console.WriteLine ($"{numProg} programmista"); 
+}
+Console.WriteLine ("enter a positive integer");
+int numProg = Convert.ToInt32 (Console.ReadLine());
+SorryForTranslit (numProg);
