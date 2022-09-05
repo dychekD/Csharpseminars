ff778c71b4cda8efd7cbc88b2eb5c52859b904d1
Daria Dychek <dychek.d@gmail.com>
2022-09-05 20:41:23 +0200

    task6

---
+++ Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿void SecondNumber (int threed)
+{
+    int second = threed / 10 % 10;
+    Console.WriteLine ($"second digit is {second}");
+}
+Console.WriteLine ("enter 3-digit number");
+int threed = Convert.ToInt32 (Console.ReadLine());
+SecondNumber (threed);
+
