edacf22826d9f1744c9b97a6b5c7a6b4f966153a
Daria Dychek <dychek.d@gmail.com>
2022-09-05 21:18:49 +0200

    task7

---
+++ Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿void ThirdDigit (int somenumber)
+{
+    if (somenumber % 100 == somenumber) Console.WriteLine ("there is no third digit");
+    else
+        {
+        int temp = somenumber;
+        while (temp > 999)
+            {
+                temp = temp / 10;       
+            }
+        Console.WriteLine ($"third digit is {temp % 10}");
+        }
+}
+Console.WriteLine ("enter a number");
+int somenumber = Convert.ToInt32 (Console.ReadLine());
+ThirdDigit (somenumber);
