79a4ffbaae3ac841b9b22eb98d26490f6b39d5ee
Daria Dychek <dychek.d@gmail.com>
2022-09-06 20:41:15 +0200

    task12

---
+++ Task12/Program.cs
@@ -0,0 +1,15 @@
+﻿string Squares (int basic)
+{
+    string result = string.Empty;
+    int count = 1;
+    double number;
+       while (count <= basic)
+    {
+        number = Math.Pow (count, 2);
+        result = result+number+", ";
+        count++;
+    }
+    return result;
+
+}
+Console.WriteLine (Squares (5));
