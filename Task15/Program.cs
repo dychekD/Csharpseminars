129c9b947e340c46153d772da0f00272a31d8b1e
Daria Dychek <dychek.d@gmail.com>
2022-09-07 19:07:56 +0200

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,29 @@
+﻿int [] arr = {1, 5, 6, 3, 2, 1, 1, 6, 8, 3};
+void PrintArray (int [] array)
+{
+    int count = array.Length;
+    for (int i=0; i < count; i++)
+    {
+        Console.Write ($"{array [i]} ");
+    }
+    Console.WriteLine ();
+}
+
+
+void SelectionSort (int [] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int MinPos = i;
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if (array [j] < array [MinPos]) MinPos=j;
+            int temporary = array [i];
+            array [i] = array [MinPos];
+            array [MinPos] = temporary;
+        }
+    }
+}
+PrintArray (arr);
+SelectionSort (arr);
+PrintArray (arr);
