3e4f37f6ec6250d5c71c9347e52ca6a45b7a62e8
Daria Dychek <dychek.d@gmail.com>
2022-09-08 17:25:51 +0200

    task16
    	# Please enter the commit message for your changes. Lines starting

---
+++ Task16/Program.cs
@@ -0,0 +1,28 @@
+﻿double distance (int xa, int ya, int za, int xb, int yb, int zb)
+{
+    double res;
+    res = Math.Sqrt ((xb - xa) * (xb - xa) + (yb - ya) * (yb - ya) + (zb - za) * (zb - za));
+    return res;
+}
+try 
+            {
+            Console.Write ("enter x coordinate for first point ");
+            int xa = Convert.ToInt32 (Console.ReadLine());
+            Console.Write ("enter y coordinate for first point ");
+            int ya = Convert.ToInt32 (Console.ReadLine());
+            Console.Write ("enter z coordinate for first point ");
+            int za = Convert.ToInt32 (Console.ReadLine());
+            Console.Write ("enter x coordinate for second point ");
+            int xb = Convert.ToInt32 (Console.ReadLine());
+            Console.Write ("enter y coordinate for second point ");
+            int yb = Convert.ToInt32 (Console.ReadLine());
+            Console.Write ("enter z coordinate for second point ");
+            int zb = Convert.ToInt32 (Console.ReadLine());
+            double rez = distance (xa, ya, za, xb, yb, zb);
+            Console.WriteLine ($"square between first point with coordinates ({xa},{ya},{za}), second pont with coordinates ({xb},{yb},{zb}) equals {rez}");
+            }
+        catch
+            {
+                Console.WriteLine("please enter integers");
+            }
+
