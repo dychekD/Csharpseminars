d033ceec1f7c8ebf218474d2425ef898956e2d5c
Daria Dychek <dychek.d@gmail.com>
2022-09-08 17:41:25 +0200

    task17

---
+++ Task17/Program.cs
@@ -0,0 +1,39 @@
+﻿string Cubes (int basic)
+{
+    string result = string.Empty;
+    double number;
+    if (basic > 0) 
+    {
+        int count = 1;
+        while (count <= basic)
+    {
+        number = Math.Pow (count, 3);
+        result = result+number+", ";
+        count++;
+    }
+    }
+    else if (basic < 0) 
+    {
+        int Mcount = -1;
+        while (Mcount >= basic)
+    {
+        number = Math.Pow (Mcount, 3);
+        result = result+number+", ";
+        Mcount = Mcount -1;
+    }
+    }
+    else Console.WriteLine ("please enter something different from 0");
+    return result;
+}
+Console.WriteLine ("enter a number");
+try
+{
+int basic = Convert.ToInt32 (Console.ReadLine());
+string Ready = Cubes (basic);
+Console.WriteLine(Ready);
+}
+catch
+{
+    Console.WriteLine("please enter integers");
+}
+
