ad00989fa0856d413d25dad8a2ce11bf556c10e3
Daria Dychek <dychek.d@gmail.com>
2022-09-09 21:15:01 +0200

    task22

---
+++ Task22/Program.cs
@@ -0,0 +1,19 @@
+﻿double Power (int A, int B)
+{
+    double result = Math.Pow (A, B);
+    return result;
+}
+Console.WriteLine ("enter first integer ");
+try
+{
+    int A = Convert.ToInt32 (Console.ReadLine());
+    Console.WriteLine ("enter second integer ");
+    int B = Convert.ToInt32 (Console.ReadLine());
+    if (B<=0) Console.WriteLine ("please enter positive integer greater than 0 as your second number");
+    else Console.WriteLine ($"Number {A} in power {B} equals {Power (A, B)}");
+}
+catch
+{
+    Console.WriteLine ("please enter integers ");
+}
+
