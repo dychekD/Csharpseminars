9cab586a1b3f1b720063350b434c60e0b677f279
Daria Dychek <dychek.d@gmail.com>
2022-09-09 13:52:35 +0200

    task19

---
+++ Task19/Program.cs
@@ -0,0 +1,18 @@
+﻿void palindrome (string dit5)
+{
+    if (dit5 [0] == dit5 [4] && dit5 [1] == dit5 [3]) Console.WriteLine ("it is a palindrome");
+    else Console.WriteLine ("it is not a palindrome");
+}
+
+
+Console.WriteLine ("please enter a 5-digit number");
+try
+{
+int number = Convert.ToInt32 (Console.ReadLine());
+string dit5 = Convert.ToString (number);
+palindrome (dit5);
+}
+catch
+{
+    Console.WriteLine ("please enter an integer");
+}
