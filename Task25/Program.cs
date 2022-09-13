7a603fd9d0f270e4bac0259426c677e72ee8d0ee
Daria Dychek <dychek.d@gmail.com>
2022-09-13 17:16:46 +0200

    task25 other solution

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,62 +1,63 @@
-﻿// int Digitss (double N)
+﻿int Digitss (decimal N)
+{   
+    int result = 0;
+    while (N % 1 > 0) N = N * 10;
+    int D = Convert.ToInt32 (N);
+    while (D > 0)
+    {
+        D = D / 10;
+        result++;
+    }
+       return result;
+}
+Console.WriteLine ("please enter a number ");
+try
+{
+    decimal N = Convert.ToDecimal (Console.ReadLine ());
+    Console.WriteLine ($"Number of digits in this number equals {Digitss (N)}");
+}
+catch
+{
+    Console.WriteLine ("please enter numbers ");
+}
+
+// int Digitss (string N)
 // {   
-//     int result = 0;
-//     while (N % 1 < 0) N = N * 10;
-//     while (N > 0)
+//     int size = N.Length;
+//     int i = 0;
+//     int coma = 0;
+//     string Inter = string.Empty;
+//     while (i< size)
 //     {
-//         N = N / 10;
+//         if (N [i] == ',') 
+//         {
+//             coma = i;
+//             for (i = 0; i<coma; i++) Inter = Inter+N [i];
+//             for (i = coma +1; i<size; i++) Inter = Inter+N [i];
+//             break;
+//         }
+//         else if (N [i] == '.') 
+//         {
+//             coma = i;
+//             for (i = 0; i<coma; i++) Inter = Inter+N [i];
+//             for (i = coma +1; i<size; i++) Inter = Inter+N [i];   
+//             break;
+//         }
+//         else i++;
+//     }
+//     int newN = 0;
+//     if (Inter != string.Empty) newN = int.Parse(Inter);
+//     else newN = int.Parse(N);
+//     int result = 0;
+//     while (newN > 0)
+//     {
+//         newN = newN / 10;
 //         result++;
 //     }
 //     return result;
 // }
 // Console.WriteLine ("please enter a number ");
-// try
-// {
-//     double N = Convert.ToDouble (Console.ReadLine ());
-//     Console.WriteLine ($"Number of digits in this number equals {Digitss (N)}");
-// }
-// catch
-// {
-//     Console.WriteLine ("please enter numbers ");
-// }
-
-int Digitss (string N)
-{   
-    int size = N.Length;
-    int i = 0;
-    int coma = 0;
-    string Inter = string.Empty;
-    while (i< size)
-    {
-        if (N [i] == ',') 
-        {
-            coma = i;
-            for (i = 0; i<coma; i++) Inter = Inter+N [i];
-            for (i = coma +1; i<size; i++) Inter = Inter+N [i];
-            break;
-        }
-        else if (N [i] == '.') 
-        {
-            coma = i;
-            for (i = 0; i<coma; i++) Inter = Inter+N [i];
-            for (i = coma +1; i<size; i++) Inter = Inter+N [i];   
-            break;
-        }
-        else i++;
-    }
-    int newN = 0;
-    if (Inter != string.Empty) newN = int.Parse(Inter);
-    else newN = int.Parse(N);
-    int result = 0;
-    while (newN > 0)
-    {
-        newN = newN / 10;
-        result++;
-    }
-    return result;
-}
-Console.WriteLine ("please enter a number ");
-string N = Console.ReadLine ();
-Console.WriteLine ($"Number of digits in this number equals {Digitss (N)}");
+// string N = Console.ReadLine ();
+// Console.WriteLine ($"Number of digits in this number equals {Digitss (N)}");
 
 
