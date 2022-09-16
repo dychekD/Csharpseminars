0f6303900ac5fc34bb9f7d0c2f049c32e446257e
Daria Dychek <dychek.d@gmail.com>
2022-09-16 22:45:50 +0200

    lines intersection

---
+++ Task34/Program.cs
@@ -0,0 +1,30 @@
+﻿string Intersection (decimal k1, decimal k2, decimal b1, decimal b2)
+{
+    string result = string.Empty;
+    if (k1 == k2 && b1 == b2) result = "your lines overlay";
+    else if (k1==k2) result = "your lines are parallel and won't intersect";
+    else 
+    {
+        decimal x = -((b1 + (-b2)) / (k1 + (-k2)));
+        decimal y = k1*x + b1;
+        result = ($"lines intersect at a point ({x};{y})");
+    }
+    return result;
+}
+try
+{
+Console.WriteLine ("There are two lines defined by functions y=k1*x+b1 and y=k2*x+b2");
+Console.WriteLine ("enter value of k1");
+decimal k1 = Convert.ToDecimal (Console.ReadLine ());
+Console.WriteLine ("enter value of b1");
+decimal b1 = Convert.ToDecimal (Console.ReadLine ());
+Console.WriteLine ("enter value of k2");
+decimal k2 = Convert.ToDecimal (Console.ReadLine ());
+Console.WriteLine ("enter value of b2");
+decimal b2 = Convert.ToDecimal (Console.ReadLine ());
+Console.WriteLine (Intersection (k1, k2, b1, b2));
+}
+catch
+{
+    Console.WriteLine ("Please enter numbers}");
+}
